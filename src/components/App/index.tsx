6e842b3156017b5cadd92f22f869d66b1d745c9d
loganwishartcraig <loganwishartcraig@gmail.com>
2019-04-06 14:21:41 -0700

    Implemented generic form components for login page

---
+++ src/components/App/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import LoginPage from '../../pages/Login';
+
+const App: React.FunctionComponent = () => (
+    <div className="App">
+        Hello
+        <LoginPage />
+    </div>
+);
+
+export default App;
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import LoginForm from '../../components/Authentication/LoginForm/LoginForm';
+
+const LoginPage: React.FunctionComponent = () => (
+
+    <div className="LoginPage">
+        <h1>Login!</h1>
+
+        <LoginForm />
+    </div>
+);
+
+export default LoginPage;
