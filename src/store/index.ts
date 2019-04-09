ce077f6772591e0afe1d620eeb57d2faaf2ddbba
loganwishartcraig <loganwishartcraig@gmail.com>
2019-04-09 09:05:00 -0700

    Integrated redux-saga and redux-logger into store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,15 @@
-import { IAuthenticationState } from '../reducers/authentication';
-import { IUserState } from '../reducers/user';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import { createLogger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
 import { AnyAppAction } from '../action-creators';
 import rootReducer from '../reducers';
+import { IAuthenticationState } from '../reducers/authentication';
+import { IUserState } from '../reducers/user';
+import { rootSaga } from '../sagas';
+import { initialized } from '../action-creators/lifecycle';
+
+export type IConnectedComponent<TStateProps = {}, TDispatchProps = {}, TOwnProps = {}> =
+    TStateProps & TDispatchProps & TOwnProps;
 
 export interface IAppState {
 
@@ -11,4 +18,20 @@ export interface IAppState {
 
 }
 
-export const store = createStore<IAppState, AnyAppAction, {}, {}>(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+const loggerMiddleware = createLogger();
+
+const middleware = [sagaMiddleware, loggerMiddleware];
+
+export const store = createStore<IAppState, AnyAppAction, {}, {}>(
+    rootReducer,
+    applyMiddleware(...middleware)
+
+);
+
+sagaMiddleware.run(rootSaga);
+
+// Must be called after sagaMiddleware.run to allow
+// sagas to be initialized
+store.dispatch(initialized());
+
